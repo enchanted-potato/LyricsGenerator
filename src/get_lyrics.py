78558674e401047fdf1a852f17ed7d090f012251
enchanted-potato <kr.karakatsani@gmail.com>
2024-05-05 18:35:20 +0100

    feat: update lyrics function

--- src/get_lyrics.py
+++ src/get_lyrics.py
@@ -1,6 +1,14 @@
 import lyricsgenius as lg
 
 
+def _save_lyrics_to_file(lyrics, path_to_txt):
+    try:
+        with open(path_to_txt, "w", encoding="utf-8") as file:
+            file.write("\n\n<|endoftext|>\n\n".join(lyrics))
+        print("Lyrics saved successfully.")
+    except Exception as e:
+        print("Failed to save lyrics:", e)
+
 def get_lyrics(
     artist,
     max_no_songs,
@@ -18,27 +26,35 @@ def get_lyrics(
         excluded_terms=excluded_terms,
         remove_section_headers=remove_section_headers,
     )
+
     try:
-        songs = (
-            genius.search_artist(artist, max_songs=int(max_no_songs), sort="popularity")
-        ).songs
+        # Attempt to search for the artist and retrieve songs
+        artist_info = genius.search_artist(artist, max_songs=int(max_no_songs), sort="popularity")
+    except Exception as e:
+        # If an exception occurs during artist search, print an error message and return None
+        print("An error occurred while searching for the artist:", e)
+        return None, None, 0
+
+    try:
+        # Extract titles and lyrics from retrieved songs
+        songs = artist_info.songs
         titles = [song.title for song in songs]
         lyrics = [song.lyrics for song in songs]
         total_no_songs = len(lyrics)
-        try:
-            file = open(path_to_txt, "w", encoding="utf-8")
-            file.write("\n\n<|endoftext|>\n\n".join(lyrics))
-            print(f"Saved lyrics")
-            return titles, lyrics, total_no_songs
-        except e:
-            print("Found lyrics but did not save text", e)
+    except Exception as e:
+        # If an exception occurs while extracting titles and lyrics, print an error message and return None
+        print("An error occurred while extracting titles and lyrics:", e)
+        return None, None, 0
 
-    except:
-        print("Failed")
-        titles = None
-        lyrics = None
-        total_no_songs = 0
-        return titles, lyrics, total_no_songs
+    try:
+        # Attempt to save lyrics to a file
+        _save_lyrics_to_file(lyrics, path_to_txt)
+    except Exception as e:
+        # If an exception occurs while saving lyrics to a file, print an error message
+        print("An error occurred while saving lyrics to a file:", e)
+
+    # If everything is successful, return titles, lyrics, and total number of songs
+    return titles, lyrics, total_no_songs
 
 
 def capitalise_list(text_list):
